2278439bdbd7321c380da28965a142676d374b0b
Xun <xun.ding@cds-global.com>
2021-12-20 19:42:04 -0600

    update tests

--- wk1/index.test.js
+++ wk1/index.test.js
@@ -1,9 +1,15 @@
 const areObjectsEqual = require("./object-comparison");
 
-describe("test", () => {
-	const testCases = [{ A: { a: 1 }, B: { a: 2 }, expected: false }];
+describe("Object comparison test", () => {
+	const testCases = [
+		{ A: { a: 1 }, B: { a: 2 }, expected: false },
+		{ A: { a: 1, b: 2 }, B: { a: 2 }, expected: false },
+		{ A: { c: 2 }, B: { a: 2 }, expected: false },
+		{ A: { x: 1, y: 1 }, B: { x: 1, y: 1 }, expected: true },
+	];
 	testCases.forEach(({ A, B, expected }) => {
-		it("test", () => {
+		it("objA and objB comparison should return correct result", () => {
+			
 			expect(areObjectsEqual(A, B)).toBe(expected);
 		});
 	});
--- wk1/object-comparison.js
+++ wk1/object-comparison.js
@@ -1,6 +1,9 @@
 function areObjectsEqual(objA, objB) {
 	// you function here
-	return true; // false
+	console.log("a=", objA);
+	console.log("B=", objB);
+	console.log(objA === objB);
+	return objA === objB; // false
 }
 
 module.exports = areObjectsEqual;
